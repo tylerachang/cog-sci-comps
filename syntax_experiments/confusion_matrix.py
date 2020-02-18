b049c72770329afc0ead18542eb34474312b1232
tylerachang98 <tylerachang98@gmail.com>
2020-02-17 19:41:38 -0600

    Output F1 scores file.

--- syntax_experiments/confusion_matrix.py
+++ syntax_experiments/confusion_matrix.py
@@ -26,9 +26,7 @@ def create_parser():
     parser.add_argument('--predictions_file')
     return parser
 
-def main(predictions_file):
-    infile = codecs.open(predictions_file, 'r')
-    
+def compute_f1(infile, outfile, language, tag_type):
     y = []
     y_hat = []
     for line in infile:
@@ -52,8 +50,9 @@ def main(predictions_file):
         # Use micro average because classes are imbalanced.
         class_f1 = metrics.f1_score(class_y, class_y_hat, average='micro')
         f1_dict[tag] = class_f1
+        outfile.write('\n{0}\t{1}\t{2}\t{3}'.format(tag_type, language, tag, class_f1))
 #        print('{0} F1: {1}'.format(tag, class_f1))
-    print(sorted(f1_dict, key=f1_dict.get))
+#    print(sorted(f1_dict, key=f1_dict.get))
     
     # Plot matrix.
     confusion = confusion_matrix(y, y_hat)
@@ -69,6 +68,52 @@ def main(predictions_file):
     plt.yticks(axis_values, labels)
     plt.show()
 #    print(confusion)
+
+def get_language_and_tag_type(filename):
+    language = 'X'
+    if 'predictions-es' in filename:
+        language = 'ES'
+    elif 'predictions-en' in filename:
+        language = 'EN'
+    elif 'predictions-ar' in filename:
+        language = 'AR'
+    elif 'predictions-ru' in filename:
+        language = 'RU'
+    elif 'predictions-fr' in filename:
+        language = 'FR'
+    elif 'predictions-zh' in filename:
+        language = 'ZH'
+    tag_type = 'X'
+    if 'tag1' in filename:
+        tag_type = 'POS'
+    elif 'tag2' in filename:
+        tag_type = 'Parent'
+    elif 'tag3' in filename:
+        tag_type = 'Grandparent'
+    elif 'tag4' in filename:
+        tag_type = 'GGrandparent'
+    return language, tag_type
+        
+
+def main(predictions_file):
+    predictions_files = []
+    if predictions_file == None or predictions_file == '':
+        models = ['predictions-es-decay-model_step_117000', 'predictions-fr-decay-model_step_147000',
+                 'predictions-en-decay-model_step_150000', 'predictions-ar-decay-model_step_149000',
+                 'predictions-ru-decay-model_step_149000', 'predictions-zh-decay-model_step_147000']
+        for experiment in range(1, 21):
+            for model in models:
+                for tag_type in range(1, 5):
+                    predictions_files.append('drive/My Drive/Cog Sci Comps/Syntax Experiment Results/Hidden and Cell Models/experiment{0}/{1}-tag{2}.txt'.format(experiment, model, tag_type))
+    else:
+        predictions_files.append(predictions_file)
+        
+    outfile = codecs.open('output.txt', 'w')
+    outfile.write('Type\tLanguage\tTag\tF1')
+    for file in predictions_files:
+        language, tag_type = get_language_and_tag_type(file)            
+        infile = codecs.open(file, 'r')
+        compute_f1(infile, outfile, language, tag_type)
 
     
 if __name__ == '__main__':
